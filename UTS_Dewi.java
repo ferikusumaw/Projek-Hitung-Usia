3bc5f54a65165c5008d985a14a51a3188601cdb3
Dewisridewi <135115787+Dewisridewi@users.noreply.github.com>
2023-06-04 17:14:59 +0700

    Add files via upload

---
+++ UTS_Dewi.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_dewi;
+
+/**
+ *
+ * @author User
+ */
+public class UTS_Dewi {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("=======================");
+        System.out.print("NAMA  : Dewi Sri Rahayu");
+        System.out.print("NIM   :2113010232");
+        System.out.print("KELAS : 4f ");
+        System.out.print ("========================");
+    }
+    
+}
