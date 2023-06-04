6d030009309d8239f45b1dcf37fd26d6b67ac46f
alfiancahyaf <53047079+alfiancahyaf@users.noreply.github.com>
2023-06-04 12:00:42 +0700

    Add files via upload

---
+++ UTS_Alfian.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alfnchyfrds_
+ */
+public class UTS_Alfian {
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("======================================");
+        System.out.println("Nama  : Alfian Cahya Firdaus");
+        System.out.println("NIM   : 2113010231");
+        System.out.println("Kelas : 4F ");
+        System.out.println("======================================");
+}
+}
